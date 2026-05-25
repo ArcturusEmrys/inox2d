3ff7f0eec6810d5e25a47b4d839ffc285111b814
Arcturus Emrys <aemrys@arcturus.live>
2026-05-25 14:44:11 +0000

    Add a test case and benchmark for the deform calculations

---
+++ inox2d/benches/interp.rs
@@ -0,0 +1,102 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use glam::Vec2;
+use inox2d::math::interp::{bi_interpolate_vec2s_additive, InterpRange, InterpolateMode};
+use rand::{random, random_range};
+
+fn random_vec2_data(size: usize) -> Vec<Vec2> {
+	let mut out = Vec::with_capacity(size);
+	for _ in 0..size {
+		out.push(Vec2::new(random(), random()));
+	}
+
+	out
+}
+
+fn bi_interpolate_vec2s_additive_bench(criterion: &mut Criterion) {
+	criterion.bench_function("bi_interpolate_vec2s_additive_nearest", |b| {
+		b.iter_batched(
+			|| {
+				let in_top_left = random_vec2_data(1_000_000);
+				let in_top_right = random_vec2_data(1_000_000);
+				let in_bottom_left = random_vec2_data(1_000_000);
+				let in_bottom_right = random_vec2_data(1_000_000);
+				let mut out = Vec::with_capacity(1_000_000);
+				out.resize(1_000_000, Vec2::ZERO);
+
+				let in_top = InterpRange::new(in_top_left, in_top_right);
+				let in_bottom = InterpRange::new(in_bottom_left, in_bottom_right);
+
+				let range_in = InterpRange::new(
+					Vec2::new(random_range(-1.0..0.0), random_range(-1.0..0.0)),
+					Vec2::new(random_range(0.0..1.0), random_range(0.0..1.0)),
+				);
+
+				let t = Vec2::new(
+					random_range(range_in.beg.x..range_in.end.x),
+					random_range(range_in.beg.y..range_in.end.y),
+				);
+
+				(t, range_in, in_top, in_bottom, out)
+			},
+			|(t, range_in, in_top, in_bottom, mut out)| {
+				let in_top_slice = InterpRange::new(in_top.beg.as_slice(), in_top.end.as_slice());
+				let in_bottom_slice = InterpRange::new(in_bottom.beg.as_slice(), in_bottom.end.as_slice());
+
+				bi_interpolate_vec2s_additive(
+					t,
+					range_in,
+					in_top_slice,
+					in_bottom_slice,
+					InterpolateMode::Nearest,
+					out.as_mut_slice(),
+				);
+			},
+			criterion::BatchSize::SmallInput,
+		);
+	});
+
+	criterion.bench_function("bi_interpolate_vec2s_additive_linear", |b| {
+		b.iter_batched(
+			|| {
+				let in_top_left = random_vec2_data(1_000_000);
+				let in_top_right = random_vec2_data(1_000_000);
+				let in_bottom_left = random_vec2_data(1_000_000);
+				let in_bottom_right = random_vec2_data(1_000_000);
+				let mut out = Vec::with_capacity(1_000_000);
+				out.resize(1_000_000, Vec2::ZERO);
+
+				let in_top = InterpRange::new(in_top_left, in_top_right);
+				let in_bottom = InterpRange::new(in_bottom_left, in_bottom_right);
+
+				let range_in = InterpRange::new(
+					Vec2::new(random_range(-1.0..0.0), random_range(-1.0..0.0)),
+					Vec2::new(random_range(0.0..1.0), random_range(0.0..1.0)),
+				);
+
+				let t = Vec2::new(
+					random_range(range_in.beg.x..range_in.end.x),
+					random_range(range_in.beg.y..range_in.end.y),
+				);
+
+				(t, range_in, in_top, in_bottom, out)
+			},
+			|(t, range_in, in_top, in_bottom, mut out)| {
+				let in_top_slice = InterpRange::new(in_top.beg.as_slice(), in_top.end.as_slice());
+				let in_bottom_slice = InterpRange::new(in_bottom.beg.as_slice(), in_bottom.end.as_slice());
+
+				bi_interpolate_vec2s_additive(
+					t,
+					range_in,
+					in_top_slice,
+					in_bottom_slice,
+					InterpolateMode::Linear,
+					out.as_mut_slice(),
+				);
+			},
+			criterion::BatchSize::SmallInput,
+		);
+	});
+}
+
+criterion_main!(benches);
+criterion_group!(benches, bi_interpolate_vec2s_additive_bench);
